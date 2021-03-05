66a0acc7932dcfad9922b12e25ac80ef86e7132e
wokeisha <oleggladilovwork@gmail.com>
2021-03-05 06:46:12 +0500

    routing implemented

--- src/core/utils.js
+++ src/core/utils.js
@@ -38,16 +38,24 @@ export function toInlineStyles(styles = {}) {
       .map(key => `${camelToDashCase(key)}: ${styles[key]}`)
       .join(';')
 }
+
 export function debounce(fn, wait) {
   let timeout
   return function(...args) {
     const later = () => {
       clearTimeout(timeout)
-        //eslint-disable-next-line
-        fn.apply(this, args)
-        // fn(...args)
+      // eslint-disable-next-line
+      fn.apply(this, args)
     }
     clearTimeout(timeout)
     timeout = setTimeout(later, wait)
   }
+}
+
+export function clone(obj) {
+  return JSON.parse(JSON.stringify(obj))
+}
+
+export function preventDefault(event) {
+  event.preventDefault()
 }
